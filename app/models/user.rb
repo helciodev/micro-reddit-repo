61d38ce3639a5b5686d030a72b9d330084db3998
rayking12 <rayodo667@gmail.com>
2021-01-20 11:26:38 +0100

    fixed comments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many:posts
+  has_many :posts
+  has_many :comments
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, length: { minimum: 10 }
 end
